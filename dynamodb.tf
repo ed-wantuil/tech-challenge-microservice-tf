836d2b3729bb02bda50387658188c0532cefabe9
Ed Wantuil <ed@wantuil.com>
2024-05-12 17:06:11 -0300

    Update DynamoDB table configuration and add new table
    Updated 'name' and 'attribute_name' in the existing DynamoDB "example_table" to "payment" and "orderId" respectively. Additionally, a new DynamoDB table named "delivery" with the same attribute configurations as the updated table was added.

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,15 +1,31 @@
 resource "aws_dynamodb_table" "example_table" {
-  name           = "example_table"
+  name           = "payment"
   read_capacity  = 5
   write_capacity = 5
 
   attribute {
-    name = "id"
+    name = "orderId"
     type = "S"
   }
 
   key_schema {
-    attribute_name = "id"
+    attribute_name = "orderId"
+    key_type       = "HASH"
+  }
+}
+
+resource "aws_dynamodb_table" "example_table" {
+  name           = "delivery"
+  read_capacity  = 5
+  write_capacity = 5
+
+  attribute {
+    name = "orderId"
+    type = "S"
+  }
+
+  key_schema {
+    attribute_name = "orderId"
     key_type       = "HASH"
   }
 }
