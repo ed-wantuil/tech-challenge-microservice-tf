3166016ec02fb95e8478ebab825834d3756e1b69
Ed Wantuil <ed@wantuil.com>
2024-05-12 18:14:15 -0300

    Update secrets and variables in terraform files
    The 'aws_secretsmanager_secret_version' resource in secrets.tf file has been updated to use 'master_username' and 'master_password' variables instead of the previous 'database_username' and 'database_password'. Also, the redundant 'database_username' and 'database_password' variables were removed from the variables.tf file.

--- secrets.tf
+++ secrets.tf
@@ -6,7 +6,7 @@ resource "aws_secretsmanager_secret" "techchallenge" {
 resource "aws_secretsmanager_secret_version" "techchallenge" {
   secret_id     = aws_secretsmanager_secret.techchallenge.id
   secret_string = jsonencode({
-    username = var.database_username
-    password = var.database_password
+    username = var.master_username
+    password = var.master_password
   })
 }
--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable "eks_version" {
   default = "1.29"
 }
 
-variable "database_username" {}
-
-variable "database_password" {}
-
 variable "master_username" {}
 
 variable "master_password" {}
