bebec3e0d9e96b8b21de81c0177b9f6b3bbab1eb
Ed Wantuil <ed@wantuil.com>
2024-05-12 21:42:34 -0300

    Update ARNs, subnet IDs, and security group IDs
    The commit updates the ARNs, subnet IDs, and security group IDs in the AWS Lambda, EKS cluster, and RDS configuration files. This change ensures that the infrastructure resources are correctly configured to reflect the updated environment settings.

--- sqs.tf
+++ sqs.tf
@@ -1,5 +1,5 @@
-resource "aws_sqs_queue" "tech_challenge_pedido" {
-  name                      = "tech_challenge_pedido"
+resource "aws_sqs_queue" "tech_challenge_producao" {
+  name                      = "tech_challenge_producao"
   delay_seconds             = 90
   max_message_size          = 2048
   message_retention_seconds = 86400
